11a30440de13a9362526b669ed5ae173b5ab7b3c
Jason Dorie <jason.dorie+github@gmail.com>
2015-10-22 01:58:37 -0700

    Updated constants to include ClockFreq

--- Firmware-C/Constants.h
+++ Firmware-C/Constants.h
@@ -3,10 +3,12 @@
 //
 //Object used to specify cross-module constants, such as pin assigments, update rates, etc
 
-#include <Propeller.h>
+#include <propeller.h>
+
+#define Const_ClockFreq  80000000
 
 #define Const_UpdateRate  250
-#define Const_UpdateCycles (80000000 / Const_UpdateRate)
+#define Const_UpdateCycles (Const_ClockFreq / Const_UpdateRate)
 
 #define Const_OneG  4096					//Must match the scale of the accelerometer
 #define Const_Alti_UpdateRate  25			//Must match the update rate of the device  
