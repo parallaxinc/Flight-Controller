aabf37d926b9b8cc7843372bcd5cb390c16057e1
Jason Dorie <jason.dorie+github@gmail.com>
2015-11-01 23:55:41 -0800

    Fixed tabs in Servo32-HighRes

--- Firmware-C/servo32_highres.cpp
+++ Firmware-C/servo32_highres.cpp
@@ -6,11 +6,11 @@
 
 
 struct ServoData {
- long FastPins, SlowPins;
- long MasterLoopDelay, SlowUpdateCounter;
- long Cycles;
- long ServoData[32];		//Servo Pulse Width information
- } Data;
+  long FastPins, SlowPins;
+  long MasterLoopDelay, SlowUpdateCounter;
+  long Cycles;
+  long ServoData[32];		//Servo Pulse Width information
+} Data;
 
 //10 clocks is the smallest amount we can wait - everything else is based on that.
 //If you're using a different clock speed, your center point will likely need to be adjusted
@@ -18,41 +18,41 @@ static const int Scale = 10;
 
 void Servo32_Start(void)
 {
-    use_cog_driver(servo32_highres_driver);
-    load_cog_driver(servo32_highres_driver, &Data);
+  use_cog_driver(servo32_highres_driver);
+  load_cog_driver(servo32_highres_driver, &Data);
 }
 
 
 void Servo32_Init( int fastRate )
 {
-	Data.MasterLoopDelay = Const_ClockFreq / fastRate;
-	Data.SlowUpdateCounter = (Const_ClockFreq / 50) / Data.MasterLoopDelay;
+  Data.MasterLoopDelay = Const_ClockFreq / fastRate;
+  Data.SlowUpdateCounter = (Const_ClockFreq / 50) / Data.MasterLoopDelay;
 
-  memset(&Data.ServoData, 0, sizeof(Data.ServoData) );
+  memset(&Data.ServoData[0], 0, sizeof(Data.ServoData) );
 
-	Data.FastPins = 0;
-	Data.SlowPins = 0;
+  Data.FastPins = 0;
+  Data.SlowPins = 0;
 }
 
 
 //Set a PIN index as a high-speed output (250Hz)  
 void Servo32_AddFastPin( int Pin )
 {
-	Data.FastPins |= (1<<Pin);                                            
-	Data.SlowPins |= (1<<Pin);	//Fast pins are also updated during the "slow" cycle                                              
+  Data.FastPins |= (1<<Pin);                                            
+  Data.SlowPins |= (1<<Pin);	//Fast pins are also updated during the "slow" cycle                                              
 }
 
 
 //Set a PIN index as a standard-speed output (50Hz)  
 void Servo32_AddSlowPin( int Pin )
 {
-	Data.SlowPins |= (1<<Pin);	//Slow pins are ONLY updated during the "slow" cycle
+  Data.SlowPins |= (1<<Pin);	//Slow pins are ONLY updated during the "slow" cycle
 }
 
 
 void Servo32_Set( int ServoPin, int Width )		// Set Servo value as a raw delay, in 10 clock increments
 {
-	Data.ServoData[ServoPin] = Width * Scale;		// Servo widths are set in 10ths of a uS, so 8000 = min, 12000 = mid, 16000 = max
+  Data.ServoData[ServoPin] = Width * Scale;		// Servo widths are set in 10ths of a uS, so 8000 = min, 12000 = mid, 16000 = max
 }
 
 /*
