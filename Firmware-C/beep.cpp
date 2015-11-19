e820fa109ba82e01bb40aff2cbf37d5be70593b3
Jason Dorie <jason.dorie+github@gmail.com>
2015-11-18 20:55:51 -0800

    Added ability to run the beeper from a system counter
    The low-voltage alarm wasn't loud enough, and I can't sit in the main
    flight loop toggling the pin, so I added code to drive it from the
    counters when necessary.

--- Firmware-C/beep.cpp
+++ Firmware-C/beep.cpp
@@ -82,3 +82,62 @@ void Beep3(void)
   waitcnt( 5000000 + CNT );
   Beep();
 }
+
+
+static int fraction( int a, int b, int shift)
+{
+  int f;
+
+  if(shift > 0)                         // if shift, pre-shift a or b left
+    a <<= shift;                        // to maintain significant bits while 
+  if(shift < 0)                         // insuring proper result
+    b <<= -shift;
+
+  for( int i=0; i<32; i++ )             // perform long division of a/b
+  {
+    f <<= 1;
+    if( a >= b )
+    {
+      a -= b;
+      f++;
+    }      
+    a <<= 1;
+  }
+  return f;
+}  
+
+
+void BeepOn(int CtrAB, int Pin, int Freq)
+{
+  int s, d, ctr, frq;
+
+  //Freq = Freq #> 0 <# 500_000         // limit frequency range
+  
+  ctr = 4 << 26;                        // ..set NCO mode
+  s = 1;                                // ..shift = 1
+
+  frq = fraction(Freq, CLKFREQ, s);     // Compute FRQA/FRQB value
+  ctr |= Pin;                           // set PINA to complete CTRA/CTRB value
+
+  if(CtrAB == 'A' )
+  {
+     CTRA = ctr;                        // set CTRA
+     FRQA = frq;                        // set FRQA                   
+  }
+
+  else if( CtrAB == 'B' )
+  {
+     CTRB = ctr;                        // set CTRB
+     FRQB = frq;                        // set FRQB                   
+  }
+  DIRA |= (1<<Pin);                    // make pin output
+}
+
+void BeepOff( int CtrAB )
+{
+  if( CtrAB == 'A' )
+    CTRA = 0;
+  else if (CtrAB == 'B' ) {
+    CTRB = 0;
+  }    
+}
