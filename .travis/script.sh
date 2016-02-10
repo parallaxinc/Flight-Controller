e68621bf72c4d39f95397eb643385c1847deed48
Jason Dorie <jason.dorie+github@gmail.com>
2016-02-09 22:35:09 -0800

    Fix moar scripts...

--- .travis/script.sh
+++ .travis/script.sh
@@ -7,17 +7,17 @@ case "$PLATFORM" in
     packthing -h
 
     packthing -j4 dmg
-    mv build/staging/flightcontroller-*.dmg .
+    mv build/staging/elev8-flightcontroller-*.dmg .
     ;;
 "linux")
     qmake -v
     packthing -h
 
     packthing -j4 run
-    mv build/staging/flightcontroller-*.run .
+    mv build/staging/elev8-flightcontroller-*.run .
 
     fakeroot packthing -j4 deb
-    mv build/staging/flightcontroller-*.deb .
+    mv build/staging/elev8-flightcontroller-*.deb .
     ;;
 *)
     echo "Invalid PLATFORM"
