c278bbf4a8b37cba6417baffd4a99e5cc4d70cc8
Jason Dorie <jason.dorie+github@gmail.com>
2016-02-11 17:53:36 -0800

    Modifying name to Parallax Groundstation for installers

--- .travis/script.sh
+++ .travis/script.sh
@@ -7,17 +7,17 @@ case "$PLATFORM" in
     packthing -h
 
     packthing -j4 dmg
-    mv build/staging/elev8-flightcontroller-*.dmg .
+    mv build/staging/elev8-groundstation-*.dmg .
     ;;
 "linux")
     qmake -v
     packthing -h
 
     packthing -j4 run
-    mv build/staging/elev8-flightcontroller-*.run .
+    mv build/staging/elev8-groundstation-*.run .
 
     fakeroot packthing -j4 deb
-    mv build/staging/elev8-flightcontroller-*.deb .
+    mv build/staging/elev8-groundstation-*.deb .
     ;;
 *)
     echo "Invalid PLATFORM"
