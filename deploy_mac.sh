39e5af04f0a3e54749b30d006bf2b60ecf52276a
Jason Dorie <jasondorie@Jasons-MacBook-Pro.local>
2016-11-20 23:05:31 -0800

    Modified mac deploy script for Qt 5.5

--- deploy_mac.sh
+++ deploy_mac.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
-cd build-GroundStation-Desktop_Qt_5_6_0_clang_64bit-Release
-~/Qt/5.6/Clang_64/bin/macdeployqt groundstation.app -dmg
+cd build-GroundStation-Desktop_Qt_5_5_1_clang_64bit-Release
+~/Qt/5.5/Clang_64/bin/macdeployqt groundstation.app -dmg
 cd ..
